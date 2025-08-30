7418ee7279612c3bdbdb7814c14df75f3432ce2f
Nenko Baldzhiev <nenko.baldzhiev@gmail.com>
2025-08-30 21:06:37 +0300

    add support for selecting device type

--- PlaywrightTests/BalanceUpdates.cs
+++ PlaywrightTests/BalanceUpdates.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Playwright.NUnit;
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
 
 using PlaywrightTests.Utils;
 
@@ -6,22 +7,55 @@ namespace PlaywrightTests;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
-public class BalanceUpdates : PageTest
+public class BalanceUpdates
 {
+
+  private IPlaywright _playwright;
+  private IBrowser _browser;
+  private IBrowserContext _context;
+  private IPage _page;
+
   /// <summary>
+  /// Creates a new browser & browser context depending on the selected device type.
   /// Open the homepage of the Irish Wilds game, starts the game and verifies the initial balances before each test.
   /// </summary>
   [SetUp]
   public async Task Setup()
   {
+    var headless = bool.Parse(Environment.GetEnvironmentVariable("HEADLESS") ?? "true");
+    var deviceName = Environment.GetEnvironmentVariable("DEVICE");
+
+    if (string.IsNullOrEmpty(deviceName))
+    {
+      throw new Exception("Setup failed: Argument `deviceName` cannot be null");
+    }
+
+    _playwright = await Playwright.CreateAsync();
+    _browser = await _playwright.Chromium.LaunchAsync(new()
+    {
+      Headless = headless
+    });
+    _context = await _browser.NewContextAsync(_playwright.Devices[deviceName]);
+    _page = await _context.NewPageAsync();
+  
     // set the expect timeout to 15s in order to have control over it
-    SetDefaultExpectTimeout(15_000);
+    // SetDefaultExpectTimeout(15_000);
+
+    await Home.OpenHomepage(_page);
+    await Home.StartGame(_page);
   
-    await Home.OpenHomepage(Page);
-    await Home.StartGame(Page);
-  
-    await GameActions.AssertBalanceAmountIsCorrect(page: Page, amount: Settings.InitialBalanceAmount);
-    await GameActions.AssertWinAmountIsCorrect(page: Page, amount: "0.00");
+    await GameActions.AssertBalanceAmountIsCorrect(page: _page, amount: Settings.InitialBalanceAmount);
+    await GameActions.AssertWinAmountIsCorrect(page: _page, amount: "0.00");
+  }
+
+  /// <summary>
+  /// Gracefully close up the browser context & browser itself after creating them manually in the setup phase.
+  /// </summary>
+  [TearDown]
+  public async Task Teardown()
+  {
+    await _context.CloseAsync();
+    await _browser.CloseAsync();
   }
 
   /// <summary>
@@ -30,6 +64,6 @@ public class BalanceUpdates : PageTest
   [Test]
   public async Task Win()
   {
-    await GameActions.TriggerSpin(Page);
+    await GameActions.TriggerSpin(_page);
   }
 }
